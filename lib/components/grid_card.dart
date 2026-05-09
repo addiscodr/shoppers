b6a956cc98b8f16b8c2404f6e2639d346c0651f6
Esayas F. Gebresilasie <addiscodr@gmail.com>
2026-05-09 12:51:13 +0300

    Add grid_card.dart to code base; Modify home.dart

---
+++ lib/components/grid_card.dart
@@ -0,0 +1,64 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/custom_theme.dart';
+
+class GridCard extends StatelessWidget {
+  final int index;
+  final void Function() onPress;
+
+  const GridCard({super.key, required this.index, required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: index % 2 == 0
+          ? const EdgeInsets.only(left: 22)
+          : const EdgeInsets.only(right: 22),
+
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(35),
+        boxShadow: CustomTheme.cardShadow,
+      ),
+      child: GestureDetector(
+        onTap: onPress,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(35),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 7,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        'https://plus.unsplash.com/premium_photo-1741194732662-5c20c90f46b8?q=80&w=1468&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Expanded(flex: 3, child:
+              Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 4,
+                      ),
+                      child: Text(
+                          "title",
+                          style: Theme.of(context).textTheme.headlineSmall),
+              ),
+                    Text("price", style: Theme.of(context).textTheme.headlineSmall)
+
+                  ]
+                )
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/grid_card.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,11 +11,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final data = ['1', '2'];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home Screen"),
 
-    );
+    return Container(
+      child: GridView.builder(
+        itemCount: data.length,
+          padding: EdgeInsets.symmetric(vertical: 30),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+          mainAxisSpacing: 30, crossAxisSpacing: 30),
+          itemBuilder: (BuildContext context, int index){
+            return GridCard(index: index, onPress:(){});
+          })
+            );
+
   }
 }
